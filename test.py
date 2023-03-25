2c427dee438bc6586d9ceec04c40baa0ce051f5d
gcesark <gcesark@outlook.com>
2023-03-25 10:54:38 -0300

    alteração do projeto

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-nota1 = int(input("digite a primeira nota do bimestre"))
-nota2 = int(input("digite a segunda nota do bimestre"))
-nota3 = int(input("digite a terceira nota do bimestre"))
-nota4 = int(input("digite a quarta nota do bimestre"))
-
-somaNota = nota1 + nota2 + nota3 + nota4 
-mediaNota = somaNota /4
-
-print(mediaNota) 
+print("alteraçãp")
