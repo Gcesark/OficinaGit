6712b1a2c023bc22b8f2f2e460e8e528b44acffa
gcesark <gcesark@outlook.com>
2023-03-25 10:04:46 -0300

    alteração

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("hello world")
+nota1 = int(input("digite a primeira nota do bimestre"))
+nota2 = int(input("digite a segunda nota do bimestre"))
+nota3 = int(input("digite a terceira nota do bimestre"))
+nota4 = int(input("digite a quarta nota do bimestre"))
+
+somaNota = nota1 + nota2 + nota3 + nota4 
+mediaNota = somaNota /4
+
+print(mediaNota) 
